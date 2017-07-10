5d56776932e0c616113c81ad2c301f903f3e3310
Vlad55rus <onlyfun55rus@gmail.com>
2017-07-10 19:43:23 +0600

    C# Calculator
    Code based on factories

---
+++ CalculatorVladDanil/CalculatorVladDanil/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CalculatorVladDanil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMulti = new System.Windows.Forms.Button();
+            this.buttonDivision = new System.Windows.Forms.Button();
+            this.textBoxEntering1 = new System.Windows.Forms.TextBox();
+            this.textBoxEntering2 = new System.Windows.Forms.TextBox();
+            this.labelOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(12, 153);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(58, 23);
+            this.buttonPlus.TabIndex = 0;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.Clicking);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(76, 153);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(58, 23);
+            this.buttonMinus.TabIndex = 1;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.Clicking);
+            // 
+            // buttonMulti
+            // 
+            this.buttonMulti.Location = new System.Drawing.Point(140, 153);
+            this.buttonMulti.Name = "buttonMulti";
+            this.buttonMulti.Size = new System.Drawing.Size(66, 23);
+            this.buttonMulti.TabIndex = 2;
+            this.buttonMulti.Text = "*";
+            this.buttonMulti.UseVisualStyleBackColor = true;
+            this.buttonMulti.Click += new System.EventHandler(this.Clicking);
+            // 
+            // buttonDivision
+            // 
+            this.buttonDivision.Location = new System.Drawing.Point(212, 153);
+            this.buttonDivision.Name = "buttonDivision";
+            this.buttonDivision.Size = new System.Drawing.Size(60, 23);
+            this.buttonDivision.TabIndex = 3;
+            this.buttonDivision.Text = "/";
+            this.buttonDivision.UseVisualStyleBackColor = true;
+            this.buttonDivision.Click += new System.EventHandler(this.Clicking);
+            // 
+            // textBoxEntering1
+            // 
+            this.textBoxEntering1.Location = new System.Drawing.Point(21, 41);
+            this.textBoxEntering1.Name = "textBoxEntering1";
+            this.textBoxEntering1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEntering1.TabIndex = 4;
+            // 
+            // textBoxEntering2
+            // 
+            this.textBoxEntering2.Location = new System.Drawing.Point(154, 41);
+            this.textBoxEntering2.Name = "textBoxEntering2";
+            this.textBoxEntering2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEntering2.TabIndex = 5;
+            // 
+            // labelOut
+            // 
+            this.labelOut.AutoSize = true;
+            this.labelOut.Location = new System.Drawing.Point(121, 98);
+            this.labelOut.Name = "labelOut";
+            this.labelOut.Size = new System.Drawing.Size(0, 13);
+            this.labelOut.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 259);
+            this.Controls.Add(this.labelOut);
+            this.Controls.Add(this.textBoxEntering2);
+            this.Controls.Add(this.textBoxEntering1);
+            this.Controls.Add(this.buttonDivision);
+            this.Controls.Add(this.buttonMulti);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMulti;
+        private System.Windows.Forms.Button buttonDivision;
+        private System.Windows.Forms.TextBox textBoxEntering1;
+        private System.Windows.Forms.TextBox textBoxEntering2;
+        private System.Windows.Forms.Label labelOut;
+    }
+}
+
---
+++ CalculatorVladDanil/CalculatorVladDanil/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculatorVladDanil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Clicking(object sender, EventArgs e)
+        {
+            var firstArgument = Convert.ToDouble(textBoxEntering1.Text);
+            var secondArgument = Convert.ToDouble(textBoxEntering2.Text);
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator(((Button)sender).Name);
+            double result = calculator.ExecuteOperation(firstArgument, secondArgument);
+            labelOut.Text = result.ToString();
+        }
+    }
+}
